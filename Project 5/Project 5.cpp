c1c627e6643d4a300b6b8016d798068745a92b7e
Kitsune <lotnik2505@gmail.com>
2021-12-28 18:32:34 +0300

    HomeWork

---
+++ Project 5/Project 5.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+//task1 
+
+bool myarr(int arr[], int size)
+{
+	for (size_t i = 0; i < size ; i++)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+	return true;
+}
+int main()
+{
+	const int size = 5;
+	int arr[] = { 1, 2, 3, 4 };
+	if (myarr (arr, size)) 
+	{
+		cout << "The array is ready" << endl;
+	}
+	return 0;
+}
+
+//task2
+
+bool Vers (int arr[], int size) 
+{
+        
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+        cout << (arr[i] = !arr[i]) << " \n";
+        
+    }
+    
+    return true;
+   
+}
+int main()
+{
+   
+    const int size = 10;
+    int arr[] = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+    Vers (arr, size);
+    
+}
+
+//task3
+
+bool Vers(int arr[], int size)
+{
+	for (size_t i = 1; i <= 22; i+=3)
+	{
+		cout << i << " ";
+	}
+	return true;
+}
+int main()
+{
+	
+	const int size = 8;
+	int arr[size];
+	if (Vers(arr, size))
+	{
+		cout << "Array Ready" << endl;
+	}
+	return 0;
+}
